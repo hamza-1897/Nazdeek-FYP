2845d90f6ef6b5dffbbaaf8756f4718ecb8bb8c3
Muhammad Hamza <hanjaa1897@gmail.com>
2026-03-25 22:21:09 +0500

    cors started

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 dotenv.config();
+app.use(cors());
 
 
 app.get('/', (req, res) => {
