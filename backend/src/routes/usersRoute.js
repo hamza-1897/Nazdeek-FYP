fea9362fc49375709544b07cce3de6bb33d5996e
Muhammad Hamza <hanjaa1897@gmail.com>
2026-03-30 00:04:16 +0500

    user signup & login implemented

--- backend/src/routes/usersRoute.js
+++ backend/src/routes/usersRoute.js
@@ -1,5 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
+const {registerUser, userLogin, userLogout, refreshAccessToken} = require('../controllers/user-authController');
+
+router.post('/register', registerUser);
+router.post('/login', userLogin);
+router.post('/logout', userLogout);
+router.get('/refresh-token', refreshAccessToken);
 
 module.exports = router;
