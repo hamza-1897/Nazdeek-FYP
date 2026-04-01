017d57ed2f382f33e684a6a2f54577a7d6639b6f
Malaika Noor <malaika9642@gmail.com>
2026-04-01 12:48:13 +0500

    resetPassword feature is installed

--- mobile-app/Navigation/auth-navigation.js
+++ mobile-app/Navigation/auth-navigation.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/loginScreen';
 import SignupScreen from '../screens/signupScreen';
-import ForgotPassword from '../screens/forgotPassword'; // New Screen
-import VerifyOTP from '../screens/verifyOTP'; // New Screen
+import ForgotPassword from '../screens/forgotPassword'; 
+import VerifyOTP from '../screens/verifyOTP'; 
+import ResetPassword from '../screens/resetPassword';
+import PasswordUpdated from '../screens/passwordUpdate';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +19,8 @@ const AuthNavigation = () => {
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="VerifyOTP" component={VerifyOTP} />
+       <Stack.Screen name="ResetPassword" component={ResetPassword} />
+        <Stack.Screen name="PasswordUpdated" component={PasswordUpdated} />
     </Stack.Navigator>
   );
 };
---
+++ mobile-app/screens/passwordUpdate.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, TouchableOpacity} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+
+const PasswordUpdated = ({ navigation }) => {
+  return (
+    <SafeAreaView className="flex-1 bg-white items-center justify-center px-8">
+      <View className="bg-[#52ab98] p-6 rounded-full mb-8 shadow-lg">
+        <Ionicons name="checkmark" size={60} color="white" />
+      </View>
+
+      <Text className="text-3xl font-bold text-gray-900 text-center">Password Updated!</Text>
+      <Text className="text-base text-gray-500 text-center mt-4 mb-16">
+        Your password has been changed successfully. You can now login with your new password.
+      </Text>
+
+      <TouchableOpacity 
+        className="bg-[#006666] w-full py-4 rounded-2xl flex-row justify-center items-center"
+        onPress={() => navigation.navigate('Login')}
+      >
+        <Text className="text-white text-lg font-bold mr-2">Back to Login</Text>
+        <Ionicons name="arrow-forward" size={20} color="white" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default PasswordUpdated;
