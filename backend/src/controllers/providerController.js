fbdd2c86a78041ba4072512be4edb72abfdc953a
Muhammad Hamza <hanjaa1897@gmail.com>
2026-04-05 23:32:56 +0500

    register provider controller

---
+++ backend/src/controllers/providerController.js
@@ -0,0 +1,48 @@
+const providerModel = require('../models/providerModel'); 
+const userModel = require('../models/usersModel');     
+
+const registerProvider = async (req, res) => {
+  try {
+    const { 
+      userId, 
+      businessName, 
+      bio, 
+      cnicNumber, 
+      address, 
+      categoryName, 
+      experience 
+    } = req.body;
+
+   
+    const cnicImages = req.files['cnicImages'] ? req.files['cnicImages'].map(f => f.path) : [];
+    const providerImage = req.files['providerImage'] ? req.files['providerImage'].map(f => f.path) : [];
+
+    
+    const newProvider = new providerModel({
+      userId,
+      businessName,
+      bio,
+      cnicNumber,
+      cnicImages,
+      providerImage,
+      address,
+      categoryName,
+      experience,
+      verificationStatus: 'pending'
+    });
+
+    await newProvider.save();
+
+    res.status(201).json({ 
+      success: true, 
+      message: "Application submit ho gayi hai. Admin verification ka intezar karein.",
+      data: newProvider 
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+module.exports = { registerProvider };
