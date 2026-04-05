495a043119544e20f7ec8e10064743976c2f699f
Muhammad Hamza <hanjaa1897@gmail.com>
2026-04-05 23:11:23 +0500

    provider schema created

---
+++ backend/src/models/providerModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+const providerSchema = new mongoose.Schema({
+ 
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true 
+  },
+  
+  businessName: {
+    type: String,
+    required: true,
+
+  },
+  
+  bio: {
+    type: String,
+    trim: true,
+    maxLength: 500
+  },
+  
+  cnicNumber: {
+    type: String,
+    required: true,
+    unique: true,
+ },
+  
+  cnicImages: {
+    type: [String],
+    required: true
+  },
+  
+  providerImage: {
+    type: [String],
+    default: []
+  },
+  
+  address: {
+    type: String,
+    required: true
+  },
+  
+  
+  categoryName: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  
+  experience: {
+    type: Number,
+    default: 0
+  },
+  
+  
+  verificationStatus: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending'
+  },
+  
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const providerModel = mongoose.model('Provider', providerSchema);
+
+module.exports = providerModel;
