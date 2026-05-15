5602561163a2d20effc959c0feac2c012eca418a
Muhammad Hamza <hanjaa1897@gmail.com>
2026-05-15 13:09:39 +0500

    booking API

--- backend/src/controllers/users/serviceBookController.js
+++ backend/src/controllers/users/serviceBookController.js
@@ -18,8 +18,45 @@ const createBooking = async (req, res) => {
     }
 };
 
+const getBookingsbyUserId = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const bookings = await bookingModel.find({ userId });
+        res.status(200).json(bookings);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const cancelBooking = async (req,res) => {
+
+    try {
+        const { bookingId } = req.params;
+        const booking = await bookingModel.findByIdAndUpdate(bookingId, { status: 'cancelled' }, { new: true });
+       res.status(200).json(booking);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        
+    }
+}
+
+const rebook = async (req,res) => {
+    try {
+        const { bookingId } = req.params;
+        const { bookingDate, bookingTime } = req.body;
+         await bookingModel.findByIdAndUpdate(bookingId, { bookingDate, bookingTime }, { new: true });
+        const booking = await bookingModel.findByIdAndUpdate(bookingId, { status: 'rebooked' }, { new: true });
+       res.status(200).json(booking);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 
 
 module.exports = {
-    createBooking
+    createBooking,
+    getBookingsbyUserId,
+    cancelBooking,
+    rebook
 };
