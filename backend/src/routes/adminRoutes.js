0794283ddc39323636b8f35245bbce484c68da52
Muhammad Hamza <hanjaa1897@gmail.com>
2026-06-28 22:19:57 +0500

    api : gett all users

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// user routes
+const {getAllUsers} = require('../controllers/admin/admin-providerController');
+router.get('/getallusers', getAllUsers);
 
 // category routes
 const {addCategory,getAllCategories} = require('../controllers/admin/categoryController');
