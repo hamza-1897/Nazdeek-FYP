6496934bd5f94b0b9836331aa85cc851af0b1dea
Muhammad Hamza <hanjaa1897@gmail.com>
2026-08-29 23:35:30 +0500

    premium benefits added

--- admin-panel/src/api/axiosInstance.js
+++ admin-panel/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
    // baseURL: 'https://nazdeek-fyp.onrender.com/api', 
-    baseURL:'http://localhost:3000/api',
+    
     withCredentials: true
 });
 
