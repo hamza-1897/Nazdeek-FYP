98a0754b488c14e0b1f4651c41ae5f88960e45d1
Muhammad Hamza <hanjaa1897@gmail.com>
2026-09-02 23:15:35 +0500

    admin accesstoken generation

--- mobile-app/api/axiosInstance.js
+++ mobile-app/api/axiosInstance.js
@@ -4,6 +4,7 @@ import * as SecureStore from 'expo-secure-store';
 
 const API_BASE_URL = 'https://nazdeek-fyp.onrender.com/api'; 
 
+//const API_BASE_URL = 'http://10.37.247.200:3000/api'; 
 
 
 const api = axios.create({
